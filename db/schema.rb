2b10316196daab790777c15e90a64e1146d95323
Cobbeec <cobbeec@gmail.com>
2020-11-28 18:25:03 -0600

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20201128235622) do
 
-  create_table "dogs", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password"
